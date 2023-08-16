1fd4b17c9a91a2cd0be18f43eed503b2c110e8b4
Anas Alaa <anasalaa@outlook.com>
2023-08-16 12:57:20 +0300

    finish error handling for signin and sign up

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middleware/validate-request';
+import 'express-async-errors'
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    res.send('Hi there!');
-});
+router.post('/api/users/signin', [
+    // Validate email and password
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('You must supply a password')
+],
+    validateRequest
+    , (req: Request, res: Response) => {
+        res.send({});
+    });
 
 export { router as signInRouter };
