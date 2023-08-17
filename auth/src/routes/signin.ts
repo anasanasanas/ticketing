e0bdb18bb465e767763238470f85d07b887dd27f
Anas Alaa <anasalaa@outlook.com>
2023-08-17 15:16:41 +0300

    finish signin router

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middleware/validate-request';
+import { User } from '../models/user';
+import { Password } from '../services/password';
+import { currentUser } from '../middleware/current-user';
+
 import 'express-async-errors'
 
 const router = express.Router();
@@ -16,9 +23,35 @@ router.post('/api/users/signin', [
         .notEmpty()
         .withMessage('You must supply a password')
 ],
-    validateRequest
-    , (req: Request, res: Response) => {
-        res.send({});
+    validateRequest,
+    currentUser,
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+
+        // Check if user exists
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw new BadRequestError('Invalid credentials');
+        }
+
+        // Check if password is correct
+        const passwordsMatch = await Password.compare(existingUser.password, password);
+        if (!passwordsMatch) {
+            throw new BadRequestError('Invalid credentials');
+        }
+
+        // Generate JWT
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        }, process.env.JWT_KEY!);
+
+        // Store JWT on session object
+        req.session = {
+            jwt: userJwt
+        };
+
+        res.status(200).send({ user: existingUser, token: userJwt });
     });
 
 export { router as signInRouter };
