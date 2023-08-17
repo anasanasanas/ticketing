134c4eedcdb378639997f8258e3f38b0ef19e63e
Anas Alaa <anasalaa@outlook.com>
2023-08-17 15:16:53 +0300

    finish signout router

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import 'express-async-errors'
 
 const router = express.Router();
 
-router.post('/api/users/signout', (req, res) => {
-    res.send('Hi there!');
+router.post('/api/users/signout', async (req, res) => {
+    req.session = null;
+
+    res.send({});
 });
 
 export { router as signOutRouter };
