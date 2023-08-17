5e33d60ae2b8edf40ac5b7a8bf708721ce5808e6
Anas Alaa <anasalaa@outlook.com>
2023-08-17 15:16:24 +0300

    add some middle ware to current user router

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import { currentUser } from '../middleware/current-user';
+import { requireAuth } from '../middleware/require-auth';
+import 'express-async-errors'
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi there!');
-});
+router.get('/api/users/currentuser',
+    currentUser,
+    requireAuth,
+    async (req, res) => {
+        res.send({ currentUser: req.currentUser || null });
+    });
 
 export { router as currentUserRouter };
