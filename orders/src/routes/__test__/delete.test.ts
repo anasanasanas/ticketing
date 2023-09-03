3599edf42c903685a568f85b4162d43c0f17b780
Anas Alaa <anasalaa@outlook.com>
2023-09-03 14:23:26 +0300

    finish unit testing

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -4,5 +4,29 @@ import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
 
 it ('marks an order as cancelled', async () => {
+    // Create a ticket with Ticket Model
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20,
+        userId: new mongoose.Types.ObjectId().toHexString()
+    });
+
+    await ticket.save();
+
+    const user = global.cookie();
+
+    // Make a request to create an order
+    const { body: order } = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({ ticketId: ticket.id })
+        .expect(201);
+
+    // Make a request to cancel the order
+    const { body: cancelledOrder } = await request(app)
+        .delete(`/api/orders/${order.id}`)
+        .set('Cookie', user)
+        .send()
+        .expect(204);
 
 });
