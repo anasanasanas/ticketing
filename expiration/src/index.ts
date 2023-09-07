5d1dd7eb5fee900ac2cd98cf4af311d554a17232
Anas Alaa <anasalaa@outlook.com>
2023-09-07 20:10:45 +0300

    Add queue and expiration queue files

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,12 @@ import { app } from './app';
 import 'express-async-errors';
 
 const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined!');
+    }
+    if (!process.env.REDIS_HOST) {
+        throw new Error('REDIS_HOST must be defined!');
+    }
     app.listen(3000, () => {
         console.log('Listening on port 3000!!');
     });
